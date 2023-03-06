ca9fc7f608fa399cc8290cf21aee8f4bcba329d0
Alejandro Silva Berbesi <me@asilvabe.dev>
2023-03-06 16:51:57 -0500

    feat: remove unnecessary index

--- database/migrations/2023_03_06_222835_create_merchants_table.php
+++ database/migrations/2023_03_06_222835_create_merchants_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('country_id')->nullable()->constrained('countries');
             $table->string('name', 90)->index();
-            $table->string('brand', 60)->nullable()->index();
+            $table->string('brand', 60)->nullable();
             $table->foreignId('document_type_id')->nullable()->constrained('document_types');
             $table->string('document', 30)->nullable();
             $table->string('website_url', 150)->nullable();
